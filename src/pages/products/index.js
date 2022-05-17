c968ac8de9b9bd0670fe954284087950a3843253
Pablo <pablolopezsorroche@gmail.com>
2022-05-17 21:08:53 +0200

    Ecommerce version 3

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,9 +1,43 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import styled from 'styled-components'
 
 import { getIsFetching, getProducts } from '../../store/products/selectors'
-
 import { fetchProductsRequestAction } from '../../store/products/actions'
+
+const ListWrapper = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  max-width: 800px;
+  margin: 0 auto;
+`
+
+const ProductInfoContainer = styled.div`
+  padding: 10px;
+`
+
+const ProductCard = styled.div`
+  border-radius: 10px;
+  box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
+`
+
+const ImageWrapper = styled.div`
+  display: block;
+  height: 0;
+  overflow: hidden;
+  padding: 100% 0 0 0;
+  position: relative;
+  width: 100%;
+`
+
+const Image = styled.img`
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`
 
 function Products () {
   const dispatch = useDispatch()
@@ -18,13 +52,20 @@ function Products () {
   if (isFetching) return 'Loading ...'
 
   return (
-    <div>
+    <ListWrapper>
       {products.map(product => (
-        <div key={product.id}>
-          {product.title}
-        </div>
+        <ProductCard key={product.id}>
+          <ImageWrapper>
+            <Image src={product.image} />
+          </ImageWrapper>
+          <ProductInfoContainer>
+            <h4>
+              {product.title}
+            </h4>
+          </ProductInfoContainer>
+        </ProductCard>
       ))}
-    </div>
+    </ListWrapper>
   )
 }
 
